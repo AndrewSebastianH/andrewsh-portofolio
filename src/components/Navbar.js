7cfac7377b8bb2bda8bb94c535e2fcf578e30a3d
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-02-27 04:15:19 +0700

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import { ReactComponent as HomeIcon } from "../icons/home.svg";
 const Navbar = () => {
   return (
     <>
-      <div className="border-b-5 ">
-        <div className="m-5 flex items-center justify-between">
+      <div className="m-5">
+        <div className="flex items-center justify-between">
           <div className="buttons gap-3 flex">
             <CustomButton>
               <HomeIcon />
@@ -18,6 +18,7 @@ const Navbar = () => {
           <div className="text-4xl font-bebas">ANDREW SEBASTIAN HARDIANTA</div>
         </div>
       </div>
+      <div className="border-black border-b-4"></div>
     </>
   );
 };
