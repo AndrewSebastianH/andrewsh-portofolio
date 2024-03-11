aab9972bc312d0309d75c77b3f39ee8743624c80
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-03-12 00:35:33 +0700

    Updated contactmecontent with svgs and buttons

--- src/components/ContactMeContent.js
+++ src/components/ContactMeContent.js
@@ -1,28 +1,39 @@
 import React from "react";
 import { constants } from "./constants/constants";
+import { ReactComponent as LinkedInIcon } from "../icons/linkedin.svg";
+import { ReactComponent as GmailIcon } from "../icons/gmail.svg";
+import { ReactComponent as GithubIcon } from "../icons/github.svg";
 
 const ContactMeContent = ({ textColor, className }) => {
   return (
     <div
       className={`w-full h-full pt-10 text-2xl text-${textColor} font-bebas ${className}`}
     >
-      <div className="flex flex-row w-full justify-between">
+      <div className="content-wrapper text-white flex flex-row w-full justify-between">
         <div>
           <a target="_blank" href={constants.linkedinURL}>
-            Linkedin
+            <button class="group h-14 w-14 hover:bg-sky-600 relative bg-sky-700 rounded text-neutral-50 duration-300 font-bold flex justify-center gap-2 items-center p-2">
+              <LinkedInIcon />
+            </button>
           </a>
         </div>
 
-        <a
-          target="_blank"
-          href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=${constants.email}`}
-        >
-          Email
-        </a>
+        <div>
+          <a
+            target="_blank"
+            href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=${constants.email}`}
+          >
+            <button className="group hover:bg-neutral-50 relative bg-neutral-300 rounded text-neutral-50 duration-100 font-bold flex justify-center gap-2 items-center p-2">
+              <GmailIcon />
+            </button>
+          </a>
+        </div>
 
         <div>
           <a target="_blank" href={constants.githubURL}>
-            GitHub
+            <button className="group icon-button hover:bg-[#222222] relative bg-neutral-50 rounded text-neutral-50 duration-100 font-bold flex justify-center gap-2 items-center p-2">
+              <GithubIcon className="icon" />
+            </button>
           </a>
         </div>
       </div>
